e5bf1be764c69636a287bfe89d19e7cfcb401b72
b-sandrine <sanrdushimimana@gmail.com>
2022-12-05 11:55:15 +0200

    Added constructor tests

--- backend/src/test/java/meme/generator/backend/model/MemeTest.java
+++ backend/src/test/java/meme/generator/backend/model/MemeTest.java
@@ -1,8 +1,17 @@
 package meme.generator.backend.model;
 
+import static org.testng.Assert.assertEquals;
+
 import org.testng.annotations.Test;
+
 
 @Test
 public class MemeTest {
+	Meme memeModel = new Meme("True story","This is it", "https://image/laugh");
 	
+	public void constructorShouldSetAllValue() {
+		assertEquals(memeModel.getHeading_text(), "True story");
+		assertEquals(memeModel.getButtom_text(), "This is it");
+		assertEquals(memeModel.getImage_url(), "https://image/laugh");
+	}
 }
